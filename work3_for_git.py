23b19969ea4eab106a01b87ed6457c02398134ff
VovkSergey <VovkSergeyN@urk.net>
2025-02-24 03:02:51 +0200

    24.02.25 codewars

--- work3_for_git.py
+++ work3_for_git.py
@@ -1,81 +1,114 @@
 # 18.02.2025
-class Server: # для описания работы серверов в сети;
-    IP_start = 1
-    def __init__(self):
-        self.buffer = []
-        self.ip = self.create_ip()
-        self.router = None
+# class Server: # для описания работы серверов в сети;
+#     IP_start = 1
+#     def __init__(self):
+#         self.buffer = []
+#         self.ip = self.create_ip()
+#         self.router = None
+#
+#     @classmethod
+#     def create_ip(cls):
+#         new_IP = cls.IP_start
+#         cls.IP_start += 1
+#         return new_IP
+#
+#     def send_data(self, data): # для отправки информационного пакета data (объекта класса Data) с указанным IP-адресом получателя (пакет отправляется роутеру и сохраняется в его буфере - локальном свойстве buffer);
+#         if not (self.router is None):
+#             self.router.buffer.append(data)
+#         else:
+#             print("Not connection with router")
+#
+#     def get_data(self): # возвращает список принятых пакетов (если ничего принято не было, то возвращается пустой список) и очищает входной буфер;
+#         push = self.buffer[:]
+#         self.buffer.clear()
+#         return push
+#
+#     def get_ip(self): # возвращает свой IP-адрес
+#         return self.ip
+#
+# class Router: # для описания работы роутеров в сети (в данной задаче полагается один роутер);
+#     def __init__(self):
+#         self.buffer = [] # список для хранения принятых от серверов пакетов (объектов класса Data)
+#         self.servers = {} # словарь серверов соединенных с роутером
+#
+#     def link(self, server): # для присоединения сервера server (объекта класса Server) к роутеру (для простоты, каждый сервер соединен только с одним роутером);
+#         self.servers[server.ip] = server
+#         if server.router is None:
+#             server.router = self
+#
+#     def unlink(self, server): # для отсоединения сервера server (объекта класса Server) от роутера;
+#         if server.ip in self.servers:
+#             del self.link_list[server.ip]
+#             server.router = None
+#
+#     def send_data(self): # для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам (после отправки буфер должен очищаться).
+#         for data_ in self.buffer:
+#             for ip, server in self.servers.items():
+#                 if data_.ip == ip:
+#                     server.buffer.append(data_.data)
+#                     break
+#
+#
+# class Data: # для описания пакета информации.
+#     def __init__(self, data: str, ip: int):
+#         self.data = data
+#         self.ip = ip
+#
+#     # def __str__(self):
+#     #     return str((self.data, self.ip))
+#     def __repr__(self):
+#         return str((self.data, self.ip))
+#
+# router = Router()
+# sv_1 = Server()
+# sv_2 = Server()
+# router.link(sv_1)
+# router.link(sv_2)
+# router.link(Server())
+# router.link(Server())
+# print(router.servers)
+# sv_5 = Server()
+# router.link(sv_5)
+# print(router.buffer)
+#
+# sv_1.send_data(Data("Hello", sv_5.get_ip()))
+# sv_2.send_data(Data("Hello", sv_5.get_ip()))
+# sv_5.send_data(Data("Hi", sv_1.get_ip()))
+# print(router.buffer)
+# router.send_data()
+# msg_lst_1 = sv_1.get_data()
+# msg_lst_5 = sv_5.get_data()
+# print(msg_lst_1, msg_lst_5, sep='\n')
 
-    @classmethod
-    def create_ip(cls):
-        new_IP = cls.IP_start
-        cls.IP_start += 1
-        return new_IP
 
-    def send_data(self, data): # для отправки информационного пакета data (объекта класса Data) с указанным IP-адресом получателя (пакет отправляется роутеру и сохраняется в его буфере - локальном свойстве buffer);
-        if not (self.router is None):
-            self.router.buffer.append(data)
-        else:
-            print("Not connection with router")
-
-    def get_data(self): # возвращает список принятых пакетов (если ничего принято не было, то возвращается пустой список) и очищает входной буфер;
-        push = self.buffer[:]
-        self.buffer.clear()
-        return push
-
-    def get_ip(self): # возвращает свой IP-адрес
-        return self.ip
-
-class Router: # для описания работы роутеров в сети (в данной задаче полагается один роутер);
-    def __init__(self):
-        self.buffer = [] # список для хранения принятых от серверов пакетов (объектов класса Data)
-        self.servers = {} # словарь серверов соединенных с роутером
-
-    def link(self, server): # для присоединения сервера server (объекта класса Server) к роутеру (для простоты, каждый сервер соединен только с одним роутером);
-        self.servers[server.ip] = server
-        if server.router is None:
-            server.router = self
-
-    def unlink(self, server): # для отсоединения сервера server (объекта класса Server) от роутера;
-        if server.ip in self.servers:
-            del self.link_list[server.ip]
-            server.router = None
-
-    def send_data(self): # для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам (после отправки буфер должен очищаться).
-        for data_ in self.buffer:
-            for ip, server in self.servers.items():
-                if data_.ip == ip:
-                    server.buffer.append(data_.data)
-                    break
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class Shape:
+   def area(self):
+      pass
+class Rectangle(Shape):
+   def __init__(self, length, breadth):
+      self.length = length
+      self.breadth = breadth
+   def area(self):
+      return self.length * self.breadth
+class Circle(Shape):
+   def __init__(self, radius):
+      self.radius = radius
+   def area(self):
+      return 3.14 * self.radius * self.radius
+   def area(self, diameter):
+      self.radius = diameter / 2
+      return 3.14 * self.radius * self.radius
+r = Rectangle(5, 10)
+print("Area of rectangle:", r.area())
+c = Circle(7)
+print("Area of circle with radius 7:", c.area())
+print("Area of circle with diameter 10:", c.area(10))
 
 
-class Data: # для описания пакета информации.
-    def __init__(self, data: str, ip: int):
-        self.data = data
-        self.ip = ip
-
-    # def __str__(self):
-    #     return str((self.data, self.ip))
-    def __repr__(self):
-        return str((self.data, self.ip))
-
-router = Router()
-sv_1 = Server()
-sv_2 = Server()
-router.link(sv_1)
-router.link(sv_2)
-router.link(Server())
-router.link(Server())
-print(router.servers)
-sv_5 = Server()
-router.link(sv_5)
-print(router.buffer)
-
-sv_1.send_data(Data("Hello", sv_5.get_ip()))
-sv_2.send_data(Data("Hello", sv_5.get_ip()))
-sv_5.send_data(Data("Hi", sv_1.get_ip()))
-print(router.buffer)
-router.send_data()
-msg_lst_1 = sv_1.get_data()
-msg_lst_5 = sv_5.get_data()
-print(msg_lst_1, msg_lst_5, sep='\n')
+x = X()
+x.sub_method(1)
+print('Обратите внимание как происходит инициализация')
+print('классов при указании аргументов в функции super()')
+y = Y()
+y.sub_method(5)
--- work4.py
+++ work4.py
@@ -1,5 +1,5 @@
-print("Hello world!!!")
-date = '18.02.2025'
+# print("Hello world!!!")
+# date = '18.02.2025'
 
 
 # # x = 0
@@ -42,18 +42,139 @@ date = '18.02.2025'
 # a = A(1, 19)
 # print(a)
 # print(a.__dict__)
-class Point2D:
-    __slots__ = ("x", "y")
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-pt2 = Point2D(10, 20)
-print(pt2.x)
-print(pt2.y)
-print(dir(pt2))
-print(pt2.__slots__)
-print(type(pt2))
-print(Point2D.__mro__)
-"\n1erwlekrjwlkrj" 
-"\n1erwlekrjwlkrj" 
-"1erwlekrjwlkrj" 
+
+# ___________________________________________________________-
+# class Point2D:
+#     __slots__ = ("x", "y")
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+# pt2 = Point2D(10, 20)
+# print(pt2.x)
+# print(pt2.y)
+# print(dir(pt2))
+# print(pt2.__slots__)
+# print(type(pt2))
+# print(Point2D.__mro__)
+# ______________________________________________________________-
+# class attr_cls:
+#     def __init__(self, value, get_change="INIT"):
+#         self.value = value
+#         self.get_change = get_change
+
+    # def getattr(self, name):
+    #     return getattr(self.value, name)
+    #
+    # def repr(self):
+    #     return str(self.value)
+    #
+    # def bool(self):
+    #     return bool(self.value)
+    #
+    # def call(self):
+    #     return self.value()
+    #
+    # def eq(self, other):
+    #     return self.value == other
+    #
+    # def add(self, other):
+    #     return self.value + other
+    #
+    # def sub(self, other):
+    #     return self.value - other
+    #
+    # def mul(self, other):
+    #     return self.value * other
+    #
+    # def truediv(self, other):
+    #     return self.value / other
+    #
+    # def radd(self, other):
+    #     return other + self.value
+    #
+    # def rsub(self, other):
+    #     return other - self.value
+    #
+    # def rmul(self, other):
+    #     return other * self.value
+    #
+    # def rtruediv(self, other):
+    #     return other / self.value
+
+# a = attr_cls(10)
+# print(a.__dict__)
+#_____________________________________________________
+def change_detection(cls):
+    class attr_cls():
+        def init(self, value, get_change='INIT'):
+            self.value = value
+            self.get_change = get_change
+
+        def getattr(self, name):
+            return getattr(self.value, name)
+
+        def repr(self):
+            return str(self.value)
+
+        def bool(self):
+            return bool(self.value)
+
+        def call(self):
+            return self.value()
+
+        def eq(self, other):
+            return self.value == other
+
+        def add(self, other):
+            return self.value + other
+
+        def sub(self, other):
+            return self.value - other
+
+        def mul(self, other):
+            return self.value * other
+
+        def truediv(self, other):
+            return self.value / other
+
+        def radd(self, other):
+            return other + self.value
+
+        def rsub(self, other):
+            return other - self.value
+
+        def rmul(self, other):
+            return other * self.value
+
+        def rtruediv(self, other):
+            return other / self.value
+
+    def getattribute(self, name):
+        try:
+            attr = super(cls, self).getattribute(name)
+            if type(attr) != attr_cls:
+                new_attr = attr_cls(attr)
+                super(cls, self).setattr(name, new_attr)
+                return new_attr
+            return attr
+        except:
+            return attr_cls(None, '')
+
+    def setattr(self, name, value):
+        target = getattr(self, name, None)
+        if type(target) != attr_cls or not target.get_change:
+            super(cls, self).setattr(name, attr_cls(value))
+        elif target.value != value or type(target.value) != type(value):
+            target.value = value
+            target.get_change = 'MOD'
+
+    def delattr(self, name):
+        target = getattr(self, name)
+        target.value = None
+        target.get_change = 'DEL'
+
+    setattr(cls, 'setattr', setattr)
+    setattr(cls, 'getattribute', getattribute)
+    setattr(cls, 'delattr', delattr)
+
+    return cls
